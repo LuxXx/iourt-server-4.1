500328556573b59301377989197f51228f0a6743
pr0v0ziert <pr0v0ziert@gmx.de>
2012-04-20 20:17:49 +0200

    removed the use of SVC_RateLimitAddress to get mod working.
    FIXME: add SVC_RateLimitAddress

--- code/server/sv_main.c
+++ code/server/sv_main.c
@@ -1081,6 +1081,10 @@ void SVC_RemoteMod(netadr_t from, msg_t * msg)
     char sv_remotemod_outputbuf[SV_REMOTEMOD_OUTPUTBUF_LENGTH];
     
     // Prevent using mod as an amplifier and make dictionary attacks impractical
+    /*
+    // LuxXx: I don't want to do more copy 'n' paste crap. I'm already tired.
+    // One day i'll add SVC_RateLimitAddress
+    // FIXME: Add SVC_RateLimitAddress
     if (SVC_RateLimitAddress(from, 10, 1000))
     {
         Com_DPrintf
@@ -1088,7 +1092,7 @@ void SVC_RemoteMod(netadr_t from, msg_t * msg)
          NET_AdrToString(from));
         return;
     }
-    
+    */
     if (sv_moderatorremoteenable->integer < 1)
         return;
     
