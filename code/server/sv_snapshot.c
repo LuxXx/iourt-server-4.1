79fd4fdf08941a86ff352bb50c566679ec1c4b9c
pr0v0ziert <pr0v0ziert@gmx.de>
2012-04-20 23:12:00 +0200

    added 64 player support

--- code/server/sv_snapshot.c
+++ code/server/sv_snapshot.c
@@ -352,8 +352,8 @@ static void SV_AddEntitiesVisibleFromPoint( vec3_t origin, clientSnapshot_t *fra
 		}
 		// entities can be flagged to be sent to a given mask of clients
 		if ( ent->r.svFlags & SVF_CLIENTMASK ) {
-			if (frame->ps.clientNum >= 32)
-				Com_Error( ERR_DROP, "SVF_CLIENTMASK: cientNum > 32\n" );
+			if (frame->ps.clientNum >= 64)
+				Com_Error( ERR_DROP, "SVF_CLIENTMASK: cientNum > 64\n" );
 			if (~ent->r.singleClient & (1 << frame->ps.clientNum))
 				continue;
 		}
