4bf93649e61c84163ee63cda4f0955c03d4a91df
pr0v0ziert <pr0v0ziert@gmx.de>
2012-05-24 21:01:31 +0200

    changed demo notice

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -787,7 +787,7 @@ void SV_Init (void) {
     
     sv_callvoteRequiredConnectTime = Cvar_Get("sv_callvoteRequiredConnectTime", "0", CVAR_ARCHIVE);
     
-    sv_demonotice = Cvar_Get ("sv_demonotice", "Smile! You're on camera!", CVAR_ARCHIVE);
+    sv_demonotice = Cvar_Get ("sv_demonotice", "[!] Smile! You're on camera!", CVAR_ARCHIVE);
 	
 	sv_callvoteCyclemapWaitTime = Cvar_Get("sv_callvoteCyclemapWaitTime", "0", CVAR_ARCHIVE);
     
